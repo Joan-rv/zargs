d3be4294379b4af5ec1eeea784898e586877c07f
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-07-04 12:14:41 +0200

    Add TODO

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use clap::Parser;
+use itertools::Itertools;
 use std::io::stdin;
 use std::process::Command;
 
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short = 'n', long, default_value_t = usize::MAX)]
-    max_args: usize,
+    max_args: usize, // TODO: handle max_args == 0
     program: String,
     #[arg(allow_hyphen_values = true)]
     arguments: Vec<String>,
@@ -13,11 +14,7 @@ struct Args {
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    for lines in stdin()
-        .lines()
-        .collect::<Result<Vec<_>, _>>()?
-        .chunks(args.max_args)
-    {
+    for lines in &stdin().lines().map_while(|x| x.ok()).chunks(args.max_args) {
         Command::new(&args.program)
             .args(&args.arguments)
             .args(lines)
